91a4dfc8289c41fafb46c797ef78d57c5f400507
Tinmar <martin.lecerf@hotmail.fr>
2025-07-17 12:38:43 +0200

    service nul mais fonctionnel

---
+++ src/main/java/org/example/Services/AnalysisService.java
@@ -0,0 +1,55 @@
+package org.example.Services;
+
+import org.example.DAO.ArticleDAO;
+import org.example.DAO.ClientDAO;
+import org.example.DAO.SaleDao;
+import org.example.Entities.Article;
+import org.example.Entities.Client;
+import org.example.Entities.Sale;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnalysisService {
+    private final ArticleDAO articleDAO = new ArticleDAO(Article.class);
+    private final ClientDAO clientDAO = new ClientDAO(Client.class);
+    private final SaleDao saleDAO = new SaleDao(Sale.class);
+
+    public List<Sale> getSalesByArticleId(int articleId) {
+        List<Sale> sales = saleDAO.get();
+        List<Sale> salesOfGivenArticle = new ArrayList<>();
+        for(Sale sale : sales) {
+            for(Article article : sale.getContent()){
+                if(article.getId() == articleId){
+                    salesOfGivenArticle.add(sale);
+                }
+            }
+        }
+        return salesOfGivenArticle;
+    }
+
+    public List<Sale> getSalesByClientId(int clientId) {
+        List<Sale> sales = saleDAO.get();
+        List<Sale> salesOfGivenClient = new ArrayList<>();
+        for(Sale sale : sales) {
+            if(sale.getClient().getId() == clientId){
+                salesOfGivenClient.add(sale);
+            }
+        }
+        return salesOfGivenClient;
+    }
+
+    public List<Sale> getSalesByPeriod(LocalDate startDate, LocalDate endDate) {
+        List<Sale> sales = saleDAO.get();
+        List<Sale> salesOfGivenPeriod = new ArrayList<>();
+        for(Sale sale : sales) {
+            if(sale.getDate().isAfter(startDate) && sale.getDate().isBefore(endDate)){
+                salesOfGivenPeriod.add(sale);
+            }
+        }
+        return salesOfGivenPeriod;
+    }
+
+
+}
